84eaf7fbe5b32d6e451404e992e231aa350a3f66
hanhquyen <hanhquyen25102004@gmail.com>
2026-04-08 11:07:30 +0700

    run.py

--- backend/run.py
+++ backend/run.py
@@ -1,17 +1,3 @@
-"""""
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
-"""""
 
 from backend.core import create_app
 
